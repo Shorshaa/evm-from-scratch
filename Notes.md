98a97c18b4cea60d8df76154a3052088c9048641
Shorshaa <jmazzonelli@gmail.com>
2022-11-16 15:57:15 -0300

    update evm.py to cover first 8 testcase

---
+++ Notes.md
@@ -0,0 +1,15 @@
+# General Information
+
+Choice for implementation: Python
+
+Links to documents used during this workshop:
+- [https://cypherpunks-core.github.io/ethereumbook/13evm.html](https://cypherpunks-core.github.io/ethereumbook/13evm.html)
+- [https://blog.trustlook.com/understand-evm-bytecode-part-1/](https://blog.trustlook.com/understand-evm-bytecode-part-1/)
+- [https://ethereum.org/pt-br/developers/docs/evm/opcodes/](https://ethereum.org/pt-br/developers/docs/evm/opcodes/)
+
+# Notes on the implementation and explanations on the choices
+
+## PUSHxx
+
+the PUSHxx OPCODES can go from PUSH1 (only 1 byte) to PUSH32 (32 bytes).
+The codes go from 0x60 (decimal 96) to 0x7F (decimal 127). So f the OPCODE is between these values and we substract 5F (decimal 95) the remainder would correspond to the number of bytes we need to PUSH to the stack
--- python/evm.py
+++ python/evm.py
@@ -13,6 +13,31 @@
 import json
 import os
 
+def push(idOP, iCode, iPc):
+    ''' This function will take care of each of the PUSH opcode
+        TO DO:
+         + better parameter naming, these just suck
+    '''
+    PUSH_BASE = 0x5F
+    nbElements = idOP-PUSH_BASE
+    print("PUSH function. Length =%s\n"%(hex(idOP)))
+
+    #Here we read each of the bytes in the Code structure and append on the previous reading 
+    # example for PUSH4 test, ltmp will have the strings '00', '11', '22', '33' appended into
+    # '00112233' then the format function on the return will transform those into valid number
+    # forcing the convertion as if that were an HEX value.
+
+    index = 0
+    ltmp = ''
+    while index < nbElements:
+        print(index)
+        ltmp += f'{iCode[iPc]:x}'
+        iPc += 1
+        index += 1
+
+    return(int(ltmp,base=16),iPc)
+
+
 def evm(code):
     pc = 0
     success = True
@@ -23,8 +48,11 @@ def evm(code):
         pc += 1
 
         # TODO: implement the EVM here!
+        if op >= 0x60 and op<=0x7F:    # This is a PUSHxx OPCODE
+           ltmp, pc = push(op, code, pc)
+           stack.append(ltmp)
         
-
+    print("strack is  %s\n"%(repr(stack)))
     return (success, stack)
 
 def test():
